a48ed39bb6abf584368eaabbf6d70a5ee4610e55
alex dam <alexdam55@gmail.com>
2022-06-30 16:59:35 -0700

    my-map in progress

--- practice-for-week-04-tdd-style-long-practice/test/my-map-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/my-map-spec.js
@@ -1,1 +1,8 @@
-// Your code here
+const { expect } = require("chai")
+const myMap = require("../problems/my-map")
+
+describe("myMap function", function () {
+    it("should return new array", function () {
+        expect(myMap(cb([1, 2, 3]))).to.equal([2, 4, 6])
+    })
+})
